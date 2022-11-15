cb231acd282a5930691d61aabafa0af0811b2712
David Kagan <davidckagan@sbcglobal.net>
2022-11-15 13:09:48 -0500

    working on auth with devise  and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'books#index'
 
   resources :books
 
-  get "/books/:id", to:"books#show"
+  #get "/books/:id", to:"books#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
